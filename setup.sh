b9038356ab67fc23a08c686988b11b0ab2084616
Conrad Hodge <me@conski.co.uk>
2023-12-10 13:25:25 +0000

    Updates
    - Fully qualify the anisble commands
    - Update node installation script - https://github.com/nodesource/distributions#installation-scripts
    - Use snap to install Go, ngrok and yq
    - Use deb822_repository for GitHub CLI repository

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 sudo apt update
 sudo apt upgrade
 sudo apt install make
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-ansible-galaxy collection install community.general
